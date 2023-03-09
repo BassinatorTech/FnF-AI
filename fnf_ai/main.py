a6d511c0a774412b67252f719acfcd4656defb63
BassinatorTech <109166734+BassinatorTech@users.noreply.github.com>
2023-03-09 10:39:29 -0600

    Update main.py

--- fnf_ai/main.py
+++ fnf_ai/main.py
@@ -1,17 +1,19 @@
 # Started: 3/8/23
 # Massive Help: sentdex
-# Videos complete: 4
+# Videos completed: 6
 # URL: https://www.newgrounds.com/portal/view/770371
+# Direct Keys: https://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
 
 import numpy
 import re
 from PIL import ImageGrab
 import cv2
 # import time
-from directKeys import *
+import keyboard
 import pyautogui
 
 
+# REGION-OF-INTEREST
 def roi(img, vertices):
     mask = numpy.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
@@ -20,29 +22,20 @@ def roi(img, vertices):
 
 
 def process_img(img):
-    processed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    processed = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     processed = cv2.Canny(processed, threshold1=200, threshold2=300)
-    vertices = numpy.array([[960, 500], [960, 0], [1920, 0], [1920, 500]])
+    processed = cv2.GaussianBlur(processed, (5, 5), 0)
+    vertices = numpy.array([[960, 900], [960, 0], [1920, 0], [1920, 900]])
     processed = roi(processed, [vertices])
     return processed
 
 
-question = input("Are you ready to awake the AI? ")
-
-if re.match("yes", question):
-    print("AI is now on")
-
-    while True:
-        screen = numpy.array(ImageGrab.grab(bbox=(0, 0, 1920, 1080)))
-        new_screen = process_img(screen)
-        cv2.imshow("window", new_screen)
-        # print("up")
-        # PressKey(w)
-        # print("release")
-        # ReleaseKey(w)
-        # cv2.imshow("window", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
-            break
-else:
-    print("Come back when you're ready")
+# COMPUTER VISION
+while True:
+    screen = numpy.array(ImageGrab.grab(bbox=(0, 0, 1920, 1080)))
+    new_screen = process_img(screen)
+    cv2.imshow("window", new_screen)
+    # cv2.imshow("window", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord("q"):
+        cv2.destroyAllWindows()
+        break

a6e3358a711f3b06cfc3253bf3e95e8814bd546f
BassinatorTech <109166734+BassinatorTech@users.noreply.github.com>
2023-03-10 19:55:07 -0600

    Update main.py

--- fnf_ai/main.py
+++ fnf_ai/main.py
@@ -31,53 +31,26 @@ def process_img(img):
     return processed
 
 
-upper_colors = {
-    "red": numpy.array([10, 255, 255]),
-    "blue": numpy.array([130, 255, 255]),
-    "purple": numpy.array([170, 255, 255]),
-    "green": numpy.array([80, 255, 255])
-}
-
-lower_colors = {
-    "red": numpy.array([0, 50, 50]),
-    "blue": numpy.array([90, 50, 50]),
-    "purple": numpy.array([130, 50, 50]),
-    "green": numpy.array([40, 50, 50])
-}
-
-
 while True:
     screen = numpy.array(ImageGrab.grab(bbox=(0, 0, 1920, 1080)))
+    # new_screen = process_img(screen)
+    file = cv2.imread("starter_img/up_arrow.png")
+    query = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    train = cv2.cvtColor(file, cv2.COLOR_BGR2GRAY)
 
-    hsv = cv2.cvtColor(screen, cv2.COLOR_BGR2HSV)
-    arrows = []
-    for color in lower_colors.keys():
-        mask = cv2.inRange(hsv, lower_colors[color], upper_colors[color])
-        contours, hierarchy = cv2.findContours(
-            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > 500:
-                x, y, w, h = cv2.boundingRect(contour)
-                if w > 20 and h > 20:
-                    arrow = {
-                        "color": color,
-                        "x": x,
-                        "y": y,
-                        "w": w,
-                        "h": h
-                    }
-                    arrows.append(arrow)
-    new_screen = process_img(screen)
-    for arrow in arrows:
-        x, y, w, h = arrow["x"], arrow["y"], arrow["w"], arrow["h"]
-        color = arrow["color"]
-        cv2.rectangle(new_screen, (x, y), (x + w, y + h), (255, 0, 0), 3)
-        cv2.putText(new_screen, color, (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    orb = cv2.ORB_create()
 
-    cv2.imshow("window", new_screen)
-    # cv2.imshow("window", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    query_kp, query_desc = orb.detectAndCompute(query, None)
+    train_kp, train_desc = orb.detectAndCompute(train, None)
+
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = matcher.match(query_desc, train_desc)
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    final = cv2.drawMatches(screen, query_kp, file,
+                            train_kp, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    cv2.imshow("window", cv2.cvtColor(final, cv2.COLOR_BGR2GRAY))
     if cv2.waitKey(25) & 0xFF == ord("q"):
         cv2.destroyAllWindows()
         break
